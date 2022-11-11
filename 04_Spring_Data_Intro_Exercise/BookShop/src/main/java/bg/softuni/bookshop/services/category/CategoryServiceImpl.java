c4c4e4d02a40c5f8941e5496860b4965f37b3579
Boris Mitov <101501574+Bohera@users.noreply.github.com>
2022-11-11 11:11:44 +0200

    Add files via upload

---
+++ 04_Spring_Data_Intro_Exercise/BookShop/src/main/java/bg/softuni/bookshop/services/category/CategoryServiceImpl.java
@@ -0,0 +1,43 @@
+package bg.softuni.bookshop.services.category;
+
+import bg.softuni.bookshop.domain.entities.Category;
+import bg.softuni.bookshop.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class CategoryServiceImpl implements CategoryService{
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void seedCategories(List<Category> categories) {
+        this.categoryRepository.saveAll(categories);
+    }
+
+    @Override
+    public boolean isDataSeeded() {
+        return this.categoryRepository.count() > 0;
+    }
+
+    @Override
+    public Set<Category> getRandomCategories() {
+        final long count = this.categoryRepository.count();
+
+        if (count != 0) {
+            Random random = new Random();
+            final long randomAuthorId = random.nextLong(1L, count) + 1L;
+
+            return Set.of(this.categoryRepository.findById(randomAuthorId).orElseThrow(NoSuchElementException::new));
+        }
+
+        throw new RuntimeException();
+    }
+}
